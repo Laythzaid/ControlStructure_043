c8224702040c219ed24a73336823953b7d8d40ec
Layth Zaid <Laithhilal23@gmail.com>
2026-09-24 11:15:21 +0700

    performance program

---
+++ program1.py
@@ -0,0 +1,11 @@
+percentage = float(input("Enter the percentage: "))
+if percentage >= 90:
+    print("Excellent")
+elif percentage >= 80:
+    print("Very good")
+elif percentage >= 70:
+    print("Good")
+elif percentage >= 60:
+    print("Average")
+else:
+    print("Poor")
